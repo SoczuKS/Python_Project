707bfebbbca7d18fd734dd235d29e3a53d65eaaf
SoczuKS <soczynskik@gmail.com>
2024-12-02 21:41:56 +0100

    Add first buttons to Window

--- Window.py
+++ Window.py
@@ -1,8 +1,8 @@
 from PySide6.QtWidgets import QWidget
-from window_form import Ui_Widget
+from window_form import Ui_MainWidget
 
 class Window(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.ui = Ui_Widget()
+        self.ui = Ui_MainWidget()
         self.ui.setupUi(self)
