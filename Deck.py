ef18d7881aaa1f857556fa753bd905c2aff815c4
SoczuKS <soczynskik@gmail.com>
2024-12-02 21:51:26 +0100

    Add Deck

---
+++ Deck.py
@@ -0,0 +1,21 @@
+import random
+from Card import Card
+from Rank import Rank
+from Suit import Suit
+
+class Deck:
+    def __init__(self):
+        self.__cards = None
+
+    def __build(self):
+        self.__cards = []
+        for suit in Suit:
+            for rank in Rank:
+                self.__cards.append(Card(suit, rank))
+
+    def shuffle(self):
+        self.__build()
+        random.shuffle(self.__cards)
+
+    def draw_card(self):
+        return self.__cards.pop()
