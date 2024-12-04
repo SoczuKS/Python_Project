17e73ab85dd4aee8c1ac50be85fa769fd1cee21f
SoczuKS <soczynskik@gmail.com>
2024-12-04 22:29:00 +0100

    Dummy slots connected to UI

--- Window.py
+++ Window.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QTableView
 from window_form import Ui_MainWidget
 
 class Window(QWidget):
@@ -6,3 +6,22 @@ class Window(QWidget):
         super().__init__(parent)
         self.ui = Ui_MainWidget()
         self.ui.setupUi(self)
+        self.ui.raise_value_slider.valueChanged.connect(self.__raise_value_slider_changed)
+
+    def connect_fold_button(self, slot):
+        self.ui.fold_button.clicked.connect(slot)
+
+    def connect_check_button(self, slot):
+        self.ui.check_button.clicked.connect(slot)
+
+    def connect_allin_button(self, slot):
+        self.ui.allin_button.clicked.connect(slot)
+
+    def connect_raise_button(self, slot):
+        self.ui.raise_button.clicked.connect(slot)
+
+    def __raise_value_slider_changed(self, value):
+        self.ui.raise_value_label.setText(f"${value}")
+
+    def get_players_table(self) -> QTableView:
+        return self.ui.players_table
