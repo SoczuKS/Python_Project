fecd6108b6fc1ffe0b54dcc7bfaa5d48e96af6cf
SoczuKS <soczynskik@gmail.com>
2024-12-04 22:27:58 +0100

    Add __str__ to Card

--- Card.py
+++ Card.py
@@ -5,3 +5,6 @@ class Card:
     def __init__(self, suit: Suit, rank: Rank):
         self.suit = suit
         self.rank = rank
+
+    def __str__(self):
+        return f"{self.rank}{self.suit.value}"
